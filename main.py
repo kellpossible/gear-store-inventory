ee950505c95c0c79f424aa6aae6c49ffd49ad294
Luke Frisken <l.frisken@gmail.com>
2016-04-16 14:11:05 +1000

    added item count

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ import json
 app = Flask(__name__)
 
 
-database_file = open("ski_store_inventory.json", "r")
-db = json.loads(database_file.read())
-database_file.close()
+class Database:
+	def __init__(self, filename):
+		database_file = open(filename, "r")
+		self.json = json.loads(database_file.read())
+		database_file.close()
+
+	def count_items_category(self, category_id):
+		count = 0
+		for item in self.json["items"]:
+			if item["category"] == category_id:
+				count += 1
+
+		return count
+
+DB = Database("ski_store_inventory.json")
 
 @app.route('/')
 def index():
-    return render_template("index.html", db=db)
+    return render_template("index.html", db=DB)
 
 if __name__ == '__main__':
 	app.debug = True
