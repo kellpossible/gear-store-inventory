692f126779c5e0473130b954a9c1b4712086607a
Luke Frisken <l.frisken@gmail.com>
2018-10-25 20:22:05 +1100

    updated to support the ski store inventory again

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ from flask_wtf.file import FileField
 from docopt import docopt
 
 
-app = Flask(__name__)
+
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%M"
 DATE_FORMAT = "%Y-%m-%d"
@@ -53,26 +53,14 @@ DATE_FORMAT = "%Y-%m-%d"
 DB = None
 
 DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-PHOTOS_DIR = "static/photo"
-app.config["SECRET_KEY"] = "SECRETKEY123"
-app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(DIRECTORY, PHOTOS_DIR)
-app.config["RESIZE_URL"] = PHOTOS_DIR
-app.config["RESIZE_ROOT"] = os.path.join(DIRECTORY, PHOTOS_DIR)
-app.config["RESIZE_NOOP"] = False
-
-Bootstrap(app)
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-PHOTOS = UploadSet('photos', flask_uploads.IMAGES)
-flask_uploads.configure_uploads(app, PHOTOS)
+PHOTOS = None
 
 USERS = {"ski@mumc.org.au": {'pw': "jfd$&Dlkj22f!"}}
 
 # command line arguments
 ARGUMENTS = None
 
-flask_resize.Resize(app)
+
 
 class EditItemForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -101,34 +89,7 @@ class NewItemForm(FlaskForm):
 class LoginUser(flask_login.UserMixin):
     pass
 
-@login_manager.user_loader
-def user_loader(email):
-    if email not in USERS:
-        return
 
-    user = LoginUser()
-    user.id = email
-    return user
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in USERS:
-        return
-
-    user = LoginUser()
-    user.id = email
-
-    # DO NOT ever store passwords in plaintext and always compare password
-    # hashes using constant-time comparison!
-
-    user.is_authenticated = request.form['pw'] == USERS[email]['pw']
-
-    return user
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return redirect(url_for('login'))
 
 class Database:
     def __init__(self, filename):
@@ -188,7 +149,7 @@ class Database:
 
     def delete_item(self, id):
         delete_item = None
-        for item in DB.json["items"]:
+        for item in self.json["items"]:
             if item["id"] == id:
                 delete_item = item
 
@@ -265,7 +226,7 @@ class Database:
             "purchase date"
             ])
 
-        for item in DB.json["items"]:
+        for item in self.json["items"]:
             writer.writerow([
                 item["id"],
                 item["name"],
@@ -286,7 +247,7 @@ class Database:
         return self.filename
 
     def get_img_src(self, image_name):
-        return "/static/photo/" + image_name
+        return self.get_photos_dir() + "/" + image_name
 
     def backup(self):
         backup_filename = "database_backup_{0}.json".format(int(time.time()))
@@ -294,178 +255,10 @@ class Database:
 
         copyfile(self.filename, backup_file_path)
 
-@app.route('/')
-@flask_login.login_required
-def index():
-    if (ARGUMENTS['new']):
-        return redirect('/new-inventory')
-    else:
-        DB.read()
-        return render_template("index.html", db=DB)
+    def get_photos_dir(self):
+        return self.json["photos-directory"]
 
-@app.route('/new-inventory')
-def new_inventory():
-    abort(404)
 
-@app.route('/new-item', methods=['GET', 'POST'])
-@flask_login.login_required
-def new_item():
-    DB.read()
-
-    form = NewItemForm()
-    form.category.choices = DB.get_category_choices()
-
-    if request.method == 'POST':
-        print("post")
-        if form.validate_on_submit():
-            print("validated")
-
-            item = DB.new_item(form.name.data)
-
-            if form.image_1.data is not None:
-                filename = PHOTOS.save(form.image_1.data)
-                item["images"].append(filename)
-                print("uploaded image 1:", filename)
-
-            if form.image_2.data is not None:
-                filename = PHOTOS.save(form.image_2.data)
-                item["images"].append(filename)
-                print("uploaded image 2:", filename)
-
-            item["category"] = int(form.category.data)
-            item["purchase_price"] = form.purchase_price.data
-            item["type"] = form.type.data
-            item["note"] = form.note.data
-            # item["currently_loaned"] = form.currently_loaned.data
-            item["quantity"] = form.quantity.data
-
-            purchase_date = form.purchase_date.data
-            if purchase_date is not None:    
-                purchase_date = datetime.datetime.combine(purchase_date, datetime.time(0, 0))
-                item["purchase_date"] = purchase_date.strftime(DATETIME_FORMAT)
-            else:
-                item["purchase_date"] = None
-             
-            DB.commit()
-
-            print("committed")
-
-            return redirect(url_for("index") + "#item-{0}".format(item["id"]))
-
-        return render_template("new_item.html", form=form, new_item_id=DB.max_id() + 1)
-    
-    if request.method == 'GET':
-        category_id = 1
-        if "category" in request.args:
-            category_id = int(request.args["category"])
-        
-        form = NewItemForm(quantity=1,
-                            purchase_price=0.0,
-                            category=category_id)
-        form.category.choices = DB.get_category_choices()
-        return render_template("new_item.html", form=form, new_item_id=DB.max_id()+1)
-
-@app.route('/edit-item/<int:id>', methods=['GET', 'POST'])
-@flask_login.login_required
-def edit_item(id):
-    DB.read()
-    item = DB.get_item(id)
-
-    if item is None:
-        abort(404)
-
-    form = EditItemForm()
-    form.category.choices = DB.get_category_choices()
-
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            item["name"] = form.name.data
-            item["category"] = form.category.data
-            item["purchase_price"] = form.purchase_price.data
-            item["type"] = form.type.data
-            item["note"] = form.note.data
-            # item["currently_loaned"] = form.currently_loaned.data
-            DB.commit()
-
-        return redirect(url_for("index") + "#item-{0}".format(item["id"]))
-
-    form = EditItemForm(
-        name=item["name"],
-        category = item["category"],
-        purchase_price = float(item["purchase_price"]),
-        quantity = item["quantity"],
-        type = item["type"],
-        note = item["note"]
-        #currently_loaned = item["currently_loaned"]
-    )
-    form.category.choices = DB.get_category_choices()
-    return render_template("edit_item.html", item=item, form=form, db=DB)
-
-@app.route('/delete-item/<int:id>', methods=['GET', 'POST'])
-@flask_login.login_required
-def delete_item(id):
-    print("ID", id)
-    DB.read()
-
-    if request.method == 'GET':
-        delete_item = DB.get_item(id)
-        if delete_item:
-            return render_template("delete_item.html", item=delete_item)
-        else:
-            return redirect(url_for("index"))
-
-    if request.method == 'POST':
-        if request.form['action'] == 'Delete':
-            DB.delete_item(id)
-            DB.commit()
-
-    return redirect(url_for("index"))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template("login.html")
-
-    email = request.form['email']
-    if request.form['pw'] == USERS[email]['pw']:
-        user = LoginUser()
-        user.id = email
-        flask_login.login_user(user)
-        next = request.args.get('next')
-
-        return redirect(next or url_for('index'))
-
-    return 'Bad login'
-
-@app.route('/download', methods=['GET'])
-def download():
-    download_format = request.args.get("format")
-
-    db_filepath = DB.get_filename()
-    db_basename = os.path.basename(db_filepath)
-    download_filename = os.path.splitext(db_basename)[0]
-
-    if download_format == "json":
-        download_filename += ".json"
-
-        return Response(
-            DB.get_json_string(),
-            mimetype="text/json",
-            headers={
-                "Content-disposition": 
-                "attachment; filename={}".format(download_filename)}
-        )
-    elif download_format == "csv":
-        download_filename += ".csv"
-        return Response(
-            DB.get_csv_string(),
-            mimetype="text/csv",
-            headers={
-                "Content-disposition": 
-                "attachment; filename={}".format(download_filename)}
-        )
-    else:
-        abort(404)
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Gear Store Inventory 0.1')
@@ -473,7 +266,226 @@ if __name__ == '__main__':
 
     if not ARGUMENTS['new']:
         DB = Database(os.path.join(DIRECTORY, ARGUMENTS["<file>"]))
+
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "SECRETKEY123"
+    app.config["UPLOADED_PHOTOS_DEST"] = os.path.join(DIRECTORY, DB.get_photos_dir())
+    app.config["RESIZE_URL"] = DB.get_photos_dir()
+    app.config["RESIZE_ROOT"] = os.path.join(DIRECTORY, DB.get_photos_dir())
+    app.config["RESIZE_NOOP"] = False
+    Bootstrap(app)
+    login_manager = flask_login.LoginManager()
+    login_manager.init_app(app)
+    PHOTOS = UploadSet('photos', flask_uploads.IMAGES)
+    flask_uploads.configure_uploads(app, PHOTOS)
+    flask_resize.Resize(app)
+    
     print(arguments)
+
+    @login_manager.user_loader
+    def user_loader(email):
+        if email not in USERS:
+            return
+
+        user = LoginUser()
+        user.id = email
+        return user
+
+    @login_manager.request_loader
+    def request_loader(request):
+        email = request.form.get('email')
+        if email not in USERS:
+            return
+
+        user = LoginUser()
+        user.id = email
+
+        # DO NOT ever store passwords in plaintext and always compare password
+        # hashes using constant-time comparison!
+
+        user.is_authenticated = request.form['pw'] == USERS[email]['pw']
+
+        return user
+
+    @login_manager.unauthorized_handler
+    def unauthorized_handler():
+        return redirect(url_for('login'))
+
+
+    @app.route('/')
+    @flask_login.login_required
+    def index():
+        if (ARGUMENTS['new']):
+            return redirect('/new-inventory')
+        else:
+            DB.read()
+            return render_template("index.html", db=DB)
+
+    @app.route('/new-inventory')
+    def new_inventory():
+        abort(404)
+
+    @app.route('/new-item', methods=['GET', 'POST'])
+    @flask_login.login_required
+    def new_item():
+        DB.read()
+
+        form = NewItemForm()
+        form.category.choices = DB.get_category_choices()
+
+        if request.method == 'POST':
+            print("post")
+            if form.validate_on_submit():
+                print("validated")
+
+                item = DB.new_item(form.name.data)
+
+                if form.image_1.data is not None:
+                    filename = PHOTOS.save(form.image_1.data)
+                    item["images"].append(filename)
+                    print("uploaded image 1:", filename)
+
+                if form.image_2.data is not None:
+                    filename = PHOTOS.save(form.image_2.data)
+                    item["images"].append(filename)
+                    print("uploaded image 2:", filename)
+
+                item["category"] = int(form.category.data)
+                item["purchase_price"] = form.purchase_price.data
+                item["type"] = form.type.data
+                item["note"] = form.note.data
+                # item["currently_loaned"] = form.currently_loaned.data
+                item["quantity"] = form.quantity.data
+
+                purchase_date = form.purchase_date.data
+                if purchase_date is not None:    
+                    purchase_date = datetime.datetime.combine(purchase_date, datetime.time(0, 0))
+                    item["purchase_date"] = purchase_date.strftime(DATETIME_FORMAT)
+                else:
+                    item["purchase_date"] = None
+                
+                DB.commit()
+
+                print("committed")
+
+                return redirect(url_for("index") + "#item-{0}".format(item["id"]))
+
+            return render_template("new_item.html", form=form, new_item_id=DB.max_id() + 1)
+        
+        if request.method == 'GET':
+            category_id = 1
+            if "category" in request.args:
+                category_id = int(request.args["category"])
+            
+            form = NewItemForm(quantity=1,
+                                purchase_price=0.0,
+                                category=category_id)
+            form.category.choices = DB.get_category_choices()
+            return render_template("new_item.html", form=form, new_item_id=DB.max_id()+1)
+
+    @app.route('/edit-item/<int:id>', methods=['GET', 'POST'])
+    @flask_login.login_required
+    def edit_item(id):
+        DB.read()
+        item = DB.get_item(id)
+
+        if item is None:
+            abort(404)
+
+        form = EditItemForm()
+        form.category.choices = DB.get_category_choices()
+
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                item["name"] = form.name.data
+                item["category"] = form.category.data
+                item["purchase_price"] = form.purchase_price.data
+                item["type"] = form.type.data
+                item["note"] = form.note.data
+                # item["currently_loaned"] = form.currently_loaned.data
+                DB.commit()
+
+            return redirect(url_for("index") + "#item-{0}".format(item["id"]))
+
+        form = EditItemForm(
+            name=item["name"],
+            category = item["category"],
+            purchase_price = float(item["purchase_price"]),
+            quantity = item["quantity"],
+            type = item["type"],
+            note = item["note"]
+            #currently_loaned = item["currently_loaned"]
+        )
+        form.category.choices = DB.get_category_choices()
+        return render_template("edit_item.html", item=item, form=form, db=DB)
+
+    @app.route('/delete-item/<int:id>', methods=['GET', 'POST'])
+    @flask_login.login_required
+    def delete_item(id):
+        print("ID", id)
+        DB.read()
+
+        if request.method == 'GET':
+            delete_item = DB.get_item(id)
+            if delete_item:
+                return render_template("delete_item.html", item=delete_item)
+            else:
+                return redirect(url_for("index"))
+
+        if request.method == 'POST':
+            if request.form['action'] == 'Delete':
+                DB.delete_item(id)
+                DB.commit()
+
+        return redirect(url_for("index"))
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template("login.html")
+
+        email = request.form['email']
+        if request.form['pw'] == USERS[email]['pw']:
+            user = LoginUser()
+            user.id = email
+            flask_login.login_user(user)
+            next = request.args.get('next')
+
+            return redirect(next or url_for('index'))
+
+        return 'Bad login'
+
+    @app.route('/download', methods=['GET'])
+    def download():
+        download_format = request.args.get("format")
+
+        db_filepath = DB.get_filename()
+        db_basename = os.path.basename(db_filepath)
+        download_filename = os.path.splitext(db_basename)[0]
+
+        if download_format == "json":
+            download_filename += ".json"
+
+            return Response(
+                DB.get_json_string(),
+                mimetype="text/json",
+                headers={
+                    "Content-disposition": 
+                    "attachment; filename={}".format(download_filename)}
+            )
+        elif download_format == "csv":
+            download_filename += ".csv"
+            return Response(
+                DB.get_csv_string(),
+                mimetype="text/csv",
+                headers={
+                    "Content-disposition": 
+                    "attachment; filename={}".format(download_filename)}
+            )
+        else:
+            abort(404)
+
+
     app.debug = True
     app.run(host=arguments['-a'], port=int(arguments['-p']))
 
